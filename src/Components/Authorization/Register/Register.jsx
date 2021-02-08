0f328e4aaf75edc538840158077351353ac3d4e2
Matin Esmaili <esmaili.matin82@gmail.com>
2021-02-08 15:35:58 +0330

    register is done but needs to be modified cuz of css reset

--- src/Components/Authorization/Register/Register.jsx
+++ src/Components/Authorization/Register/Register.jsx
@@ -1,153 +1,140 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import classes from "./css/register.module.css";
+import { Link } from "react-router-dom";
 
 class Register extends Component {
-   
-    render() { 
-        return <div>
+  render() {
+    return (
+      <div>
+        <div className={classes.shape1_holder}></div>
 
-<div class="shape1_holder">
+        <div className={classes.shape2_holder}></div>
 
-</div>
-
-<div class="shape2_holder">
-
-</div>
-
-{/* <!-- header --> */}
-<div id="menu">
-    <div class="menu-logo">
-        <img src="img/hpgraph-1.svg" />
-    </div>
-    <ul class="menu-tab">
-        <li class="below 1"> خانه
-            <div class="border-bottom"></div>
-        </li>
-        <li class="below 1"> آموزش</li>
-        <li class="below 1"> خدمات</li>
-        <li class="below 1"> مالی</li>
-        <li class="below 1"> تماس با ما</li>
-    </ul>
-
-</div>
-<div id="search-holder">
-    <input id="search-box" type="search"/>
-    <div id="user"></div>
-    <p class="little-des"> میان هزاران دوره ی آنلاین</p>
-</div>
-{/* <!-- form --> */}
-{/* <!-- responsive header --> */}
-<div class="drop_down">
-<div class="back">
-
-</div>
-<div class="dropItems sel">خانه</div>
-<span class="lineForHome"></span>
-<div class="dropItems">آموزش</div>
-<div class="dropItems">خدمات</div>
-<div class="dropItems">مالی</div>
-<div class="dropItems">تماس با ما</div>
-
-<div class="dropItemss"> <img src="img/Work.svg" alt="" class="nahyeKarbari"/>ناحیه کاربری
-
-</div>
-
-</div>
-<div class="top_header"> 
-    <div class="purse ith">
-
-    </div>
-
-    <div class="header_button ith">
-
-    </div>
-</div>
-<div class="header_text_holder">
-    <div class="header_text">ثبت نام</div>
-</div>
-    {/* <!-- responsive header --> */}
-
-<div id="form_holder">  
-    <div class="formInnerHolder"></div>
-        <div class="section1">
-            <div class="userNameTextHolder">
-                <p class="usernametext">
-                    نام و نام خانوادگی
-                </p>
-            </div>
-            <div class="userNameInputHolder1">
-                <div class="userNameInputHolder2">
-
-                    <input type="text" placeholder="متین اسماعیلی"/>
-                </div>
-            </div>
+        {/* <!-- header --> */}
+        <div id={classes.menu}>
+          <div className={classes["menu-logo"]}>
+            <img
+              src={require("../../../Assets/images/hpgraph-1.svg").default}
+            />
+          </div>
+          <ul className={classes["menu-tab"]}>
+            <li className={`${classes.below} ${classes["1"]}`}>
+              {" "}
+              خانه
+              <div className={classes["border-bottom"]}></div>
+            </li>
+            <li className={`${classes.below} ${classes["1"]}`}> آموزش</li>
+            <li className={`${classes.below} ${classes["1"]}`}> خدمات</li>
+            <li className={`${classes.below} ${classes["1"]}`}> مالی</li>
+            <li className={`${classes.below} ${classes["1"]}`}> تماس با ما</li>
+          </ul>
         </div>
-
-        <div class="section2">
-            <div class="passWordTextHolder">
-                <p class="passwordtext">
-                    ایمیل
-                </p>
-            </div>
-            <div class="passWordInputHolder1">
-                <div class="passWordInputHolder2">
-
-                    <input class="emailInputInReg" type="text" placeholder="info@bahr.com"/>
-                </div>
-            </div>
-
+        <div id={classes["search-holder"]}>
+          <input id={classes["search-box"]} type="search" />
+          <div id={classes.user}></div>
+          <p className={classes["little-des"]}> میان هزاران دوره ی آنلاین</p>
         </div>
-        <div class="section3">
-            <div class="passWordTextHolder">
-                <p class="passwordtext">
-                    رمز عبور
-                </p>
-            </div>
-            <div class="passWordInputHolder1">
-                <div class="passWordInputHolder2 pwRegister">
-                    <div class="showPass1"></div>
-                    <input id="shp1" type="password" placeholder="حداقل 8 کاراکتر"/>
-                </div>
-            </div>
+        {/* <!-- form --> */}
+        {/* <!-- responsive header --> */}
+        <div className={classes["drop_down"]}>
+          <div className={classes["back"]}></div>
+          <div className={`${classes.dropItems} ${classes.sel}`}>خانه</div>
+          <span className={classes.lineForHome}></span>
+          <div className={classes.dropItems}>آموزش</div>
+          <div className={classes.dropItems}>خدمات</div>
+          <div className={classes.dropItems}>مالی</div>
+          <div className={classes.dropItems}>تماس با ما</div>
 
+          <div className={classes.dropItems}>
+            {" "}
+            <img src={require("../../../Assets/images/Work.svg").default} alt="" className="nahyeKarbari" />
+            ناحیه کاربری
+          </div>
         </div>
-        <div class="section4">
-            <div class="passWordTextHolder passrepeat">
+        <div className={classes["top_header"]}>
+          <div className={`${classes.purse} ${classes.ith}`}></div>
 
-                <p class="passwordtext">
-                    تکرار رمز عبور
-                </p>
-            </div>
-            <div class="passWordInputHolder1">
-                <div class="passWordInputHolder2 pwRegister">
-                    <div class="showPass2"></div>
-                    <input id="shp2" type="password" placeholder="حداقل 8 کاراکتر"/>
-                </div>
-            </div>
-
+          <div className={`${classes["header_button"]} ${classes.ith}`}></div>
         </div>
+        <div className={classes["header_text_holder"]}>
+          <div className={classes["header_text"]}>ثبت نام</div>
+        </div>
+        {/* <!-- responsive header --> */}
 
+        <div id={classes["form_holder"]}>
+          <div className={classes.formInnerHolder}></div>
+          <div className={classes.section1}>
+            <div className={classes.userNameTextHolder}>
+              <p className={classes.usernametext}>نام و نام خانوادگی</p>
+            </div>
+            <div className={classes.userNameInputHolder1}>
+              <div className={classes.userNameInputHolder2}>
+                <input type="text" placeholder="متین اسماعیلی" />
+              </div>
+            </div>
+          </div>
 
-        <div class="forgetPass">
-            <div class="checkbox">
-                <input type="checkbox"/>
+          <div className={classes.section2}>
+            <div className={classes.passWordTextHolder}>
+              <p className={classes.passwordtext}>ایمیل</p>
+            </div>
+            <div className={classes.passWordInputHolder1}>
+              <div className={classes.passWordInputHolder2}>
+                <input
+                  className={classes.emailInputInReg}
+                  type="text"
+                  placeholder="info@bahr.com"
+                />
+              </div>
+            </div>
+          </div>
+          <div className={classes.section3}>
+            <div className={classes.passWordTextHolder}>
+              <p className={classes.passwordtext}>رمز عبور</p>
+            </div>
+            <div className={classes.passWordInputHolder1}>
+              <div className={`${classes["passWordInputHolder2"]} ${classes.pwRegister}`}>
+                <div className={classes.showPass1}></div>
+                <input
+                  id={classes.shp1}
+                  type="password"
+                  placeholder="حداقل 8 کاراکتر"
+                />
+              </div>
+            </div>
+          </div>
+          <div className={classes.section4}>
+            <div className={`${classes["passWordTextHolder"]} ${classes.passrepeat}`}>
+              <p className={classes.passwordtext}>تکرار رمز عبور</p>
+            </div>
+            <div className={classes.passWordInputHolder1}>
+              <div className={`${classes["passWordInputHolder2"]} ${classes.pwRegister}`}>                     
+                <div className={classes.showPass2}></div>
+                <input
+                  id={classes.shp2}
+                  type="password"
+                  placeholder="حداقل 8 کاراکتر"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className={classes.forgetPass}>
+            <div className={classes.forgetPass}>
+              <input type="checkbox" />
             </div>
             <p>مرا به خاطر بسپار</p>
+          </div>
+          <div className={classes.buttons_holder}>
+            <Link to="/Login">
+              <div className={classes.signin}>ورود</div>
+            </Link>
+            <div className={classes.signup}>ثبت نام</div>
+          </div>
         </div>
-        <div class="buttons_holder">
-            <a href="login.html">
-                <div class="signin">
-                    ورود
-                </div>
-            </a>
-            <div class="signup">
-                ثبت نام
-            </div>
-
-        </div>
-    </div>
-        </div>
-    }
+      </div>
+    );
+  }
 }
- 
+
 export default Register;
